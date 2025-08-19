85813602dac9f90f51bf1b04ae1938a860192c13
morgantstewart <morgan@paysigil.com>
2025-08-19 11:37:49 -0400

    Fix hoot.js model: move commentSchema before usage and fix mongoose export

--- models/hoot.js
+++ models/hoot.js
@@ -1,4 +1,15 @@
-// models/hoot.js
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: true
+    },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+  },
+  { timestamps: true }
+);
 
 const hootSchema = new mongoose.Schema(
   {
@@ -16,22 +27,9 @@ const hootSchema = new mongoose.Schema(
       enum: ["News", "Sports", "Games", "Movies", "Music", "Television"],
     },
     author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    comments: [commentSchema], // add here
+    comments: [commentSchema],
   },
   { timestamps: true }
 );
 
-const commentSchema = new mongoose.Schema(
-  {
-    text: {
-      type: String,
-      required: true
-    },
-    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
-  },
-  { timestamps: true }
-);
-
-
-
-module.exports = Hoot;
+module.exports = mongoose.model('Hoot', hootSchema);
